ea9ac1603d0904295b2fd21db5ca7ebe7ecd1731
Daniel Rabinkov <rabinkovdanny@gmail.com>
2021-11-12 00:37:06 +0200

    started RealEstate react webapp

---
+++ webapp/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import HomePageComp from "./pages/HomePageComp";
+
+function App() {
+  return (
+    <div className="App">
+      <HomePageComp></HomePageComp>
+    </div>
+  );
+}
+
+export default App;
---
+++ webapp/src/pages/HomePageComp.jsx
@@ -0,0 +1,14 @@
+/* import { Container, Button } from "react-bootstrap"; */
+import React from "react";
+
+export default class HomePageComp extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="container">
+          <h1>Hi Im HomePage!!</h1>
+        </div>
+      </>
+    );
+  }
+}
