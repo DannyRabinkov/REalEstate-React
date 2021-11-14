35134e964c626e13bbfc98217f45c265cf899923
Daniel Rabinkov <rabinkovdanny@gmail.com>
2021-11-15 01:18:54 +0200

    added fetch calls to server

--- webapp/src/helpers/javascriptHelpers.js
+++ webapp/src/helpers/javascriptHelpers.js
@@ -10,3 +10,107 @@ export function logout(setUserCallback) {
   localStorage.clear();
   setUserCallback(undefined);
 }
+
+let baseUrl = "http://localhost:3000";
+
+export function registerNewAccount(data, callback) {
+  let url = baseUrl + "/api/users";
+  let obj = getConfigurationForPostRequest(data);
+
+  fetch(url, obj)
+    .then((x) => x.json())
+    .then((x) => callback(x))
+    .catch((x) => callback(x));
+}
+export function creatNewCard(data, callback) {
+  let url = baseUrl + "/api/cards";
+  let obj = prepareRequest(data);
+
+  fetch(url, obj)
+    .then((response) => response.json())
+    .then((response) => callback(response))
+    .catch((error) => callback(error));
+}
+export function deleteCard(data, callback) {
+  let url = baseUrl + "/api/cards/" + data;
+  let obj = prepareDeleteRequest();
+
+  fetch(url, obj)
+    .then((response) => response.json())
+    .then((response) => callback(response))
+    .catch((error) => callback(error));
+}
+export function getUserCard(callback) {
+  let url = baseUrl + "/api/cards/all";
+  let obj = EmptyGETRequest();
+  fetch(url, obj)
+    .then((response) => response.json())
+    .then((response) => callback(response))
+    .catch((error) => callback(error));
+}
+
+export function signInUser(data, callback) {
+  let url = baseUrl + "/api/auth";
+  let obj = getConfigurationForPostRequest(data);
+  fetch(url, obj)
+    .then((x) => x.json())
+    .then((x) => callback(x))
+    .catch((x) => callback(x));
+}
+
+export function getMeData(token, callback) {
+  if (!token) return;
+  let url = baseUrl + "/api/users/me";
+  fetch(url, { headers: { "x-auth-token": token } })
+    .then((x) => x.json())
+    .then((x) => callback(x))
+    .catch((x) => callback(x));
+}
+
+function getConfigurationForPostRequest(data) {
+  return {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(data),
+  };
+}
+
+function prepareRequest(data) {
+  let token = localStorage.getItem("token");
+  return {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "x-auth-token": token,
+    },
+    method: "POST",
+    body: JSON.stringify(data),
+  };
+}
+
+function prepareDeleteRequest() {
+  let token = localStorage.getItem("token");
+  return {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "x-auth-token": token,
+    },
+    method: "DELETE",
+  };
+}
+
+function EmptyGETRequest() {
+  let token = localStorage.getItem("token");
+  return {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "x-auth-token": token,
+    },
+    method: "GET",
+  };
+}
