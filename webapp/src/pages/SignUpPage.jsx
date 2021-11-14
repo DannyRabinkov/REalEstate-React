796cfe034561740a4bef96f19008fceb5c3254f7
Daniel Rabinkov <rabinkovdanny@gmail.com>
2021-11-15 01:18:28 +0200

    added some text

--- webapp/src/pages/SignUpPage.jsx
+++ webapp/src/pages/SignUpPage.jsx
@@ -10,6 +10,7 @@ export default class SignUpPage extends React.Component {
           <h2 className="mainpageheader mt-2 mb-2">
             Sign-Up to create you'r new account! <MdAppRegistration />
           </h2>
+          <h3>Sign-up if you want to add you'r appartments! </h3>
           <SignUpComp />
         </div>
       </>
