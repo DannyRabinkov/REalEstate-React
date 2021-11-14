6817d6e47cb0ee07d7067703cde8a740d670ffca
Daniel Rabinkov <rabinkovdanny@gmail.com>
2021-11-15 00:21:52 +0200

    commit

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -3,5 +3,6 @@ import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
+import "react-toastify/dist/ReactToastify.css";
 
 render(<App />, document.getElementById("root"));
