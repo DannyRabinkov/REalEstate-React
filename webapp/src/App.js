0ff77afd8193daaddbe36be3525926e068a038ec
Daniel Rabinkov <rabinkovdanny@gmail.com>
2021-11-15 00:17:37 +0200

    hard-coded user add

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import FooterComp from "./components/footerComp/FooterComp";
 import NavBarComp from "./components/navBarComp/NavBarComp";
+import { ToastContainer } from "react-toastify";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [user, setUser] = useState(undefined);
+
+  useEffect(() => {
+    if (localStorage.getItem("token") != null)
+      setUser(localStorage.getItem("token"));
+  });
+
   return (
     <div className="App">
-      <NavBarComp />
+      <NavBarComp setUserCallback={setUser} user={user} />
+      <ToastContainer />
       <FooterComp />
     </div>
   );
