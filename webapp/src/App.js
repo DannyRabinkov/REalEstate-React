1357dce254edeb502deb2c10e1597e8f944bc442
Daniel Rabinkov <rabinkovdanny@gmail.com>
2021-11-15 23:06:45 +0200

    commit

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -3,13 +3,17 @@ import FooterComp from "./components/footerComp/FooterComp";
 import NavBarComp from "./components/navBarComp/NavBarComp";
 import { ToastContainer } from "react-toastify";
 import { useState, useEffect } from "react";
+import { getMeData } from "../src/helpers/javascriptHelpers";
 
 function App() {
   const [user, setUser] = useState(undefined);
 
   useEffect(() => {
     if (localStorage.getItem("token") != null)
-      setUser(localStorage.getItem("token"));
+      getMeData(localStorage.getItem("token"), (data) => {
+        setUser(data);
+        /* setUser(data); */
+      });
   });
 
   return (
